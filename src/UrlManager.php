a8f6ac9f948dba904eba5ef1c0acf01fba473dfc
Christian Micklisch <cmicklis@stetson.edu>
2017-03-31 23:19:09 -0400

    Updated the namespacing

--- src/UrlManager.php
+++ src/UrlManager.php
@@ -1,23 +1,31 @@
 <?php
 
 /**
+ * Contains the UrlManager class.
+ *
+ * @package MILF
+ * @author  John Salis <jsalis@stetson.edu>
+ */
+
+namespace MILF;
+
+/**
  * The UrlManager class is used to parse urls with camel case into hyphenated urls.
  *
- * @author jsalis@stetson.edu
+ * @author John Salis <jsalis@stetson.edu>
  */
-class UrlManager extends CUrlManager
-{ 
+class UrlManager extends \CUrlManager {
+
     /**
      * The createUrl method goes and generates the url by using regular expressions
      * meaning that the url and have minus signs to allow for a seperation of words.
      * 
-     * @param  String  $route      A direction to the called controller and action.
+     * @param  string  $route      A direction to the called controller and action.
      * @param  array   $params     Parameters of the url (GET's).
      * @param  string  $ampersand  Self explanatory.
-     * @return String              The url of the parent.
+     * @return string              The url of the parent.
      */
-    public function createUrl($route = "", array $params = [], $ampersand = '&')
-    {
+    public function createUrl($route = "", array $params = [], $ampersand = '&') {
         $route = preg_replace_callback('/(?<![A-Z])[A-Z]/', function($matches) {
             return '-' . lcfirst($matches[0]);
         }, $route);
@@ -29,10 +37,9 @@ class UrlManager extends CUrlManager
      * that needs to be removed (spaces and minus signs).
      *  
      * @param  CHttpRequest $request The request application component
-     * @return String                The filtered url.
+     * @return string                The filtered url.
      */
-    public function parseUrl($request)
-    {
+    public function parseUrl($request) {
         $route = parent::parseUrl($request);
         return lcfirst(str_replace(' ', '', ucwords(str_replace('-', ' ', $route))));
     }
